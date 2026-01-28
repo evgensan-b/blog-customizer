60b072ea8b435df652a38cd02333953a43481ea6
EvgenSan <evgnesan@yandex.ru>
2026-01-28 13:55:45 +0300

    feat: add sidebar and handlers

---
+++ src/app/index.tsx
@@ -0,0 +1,28 @@
+import { CSSProperties } from 'react';
+import clsx from 'clsx';
+
+import { Article } from '../components/article/Article';
+import { ArticleParamsForm } from '../components/article-params-form/ArticleParamsForm';
+import { defaultArticleState } from '../constants/articleProps';
+
+import '../styles/index.scss';
+import styles from '../styles/index.module.scss';
+
+export const App = () => {
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': defaultArticleState.fontFamilyOption.value,
+					'--font-size': defaultArticleState.fontSizeOption.value,
+					'--font-color': defaultArticleState.fontColor.value,
+					'--container-width': defaultArticleState.contentWidth.value,
+					'--bg-color': defaultArticleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm />
+			<Article />
+		</main>
+	);
+};
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,89 @@
+import { useState } from 'react';
+
+import {
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions
+} from '../../constants/articleProps';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onApply?: (params: ArticleStateType) => void;
+	onReset?: () => void;
+};
+
+export const ArticleParamsForm = ({ onApply, onReset}: ArticleParamsFormProps) => {
+	//состояние открытия/закрытия сайдбара
+	const [isOpen, setIsOpen] = useState(false);
+
+	// состояние формы (текущие значения)
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	// обработчик открытия/закрытия сайдбара
+	const handleArrowClick = () => {
+		setIsOpen(!isOpen);
+	};
+
+	// обработчик изменения полей формы
+	const handleChange = (
+		fieldName: keyof ArticleStateType,
+		value: OptionType
+	) => {
+		setFormState((prev) => ({
+			...prev,
+			[fieldName]: value,
+		}));
+	};
+
+	//обработчик применения настроек полей формы
+	const handleApply = () => {
+		onApply?.(formState);
+	};
+
+	//обработчик сброса настроек полей формы
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onReset?.();
+	};
+	// не забыть удалить!!!
+	const isOpenForDevelopment = true;
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton isOpen={isOpenForDevelopment} onClick={handleArrowClick} />
+			{isOpenForDevelopment && (
+				<aside className={`${styles.container} ${styles.container_open}`}>
+					<form className={styles.form}>
+						<div className={styles.bottomContainer}>
+							<Button
+								title='Сбросить'
+								htmlType='reset'
+								type='clear'
+								onClick={handleReset}
+							/>
+							<Button
+								title='Применить'
+								htmlType='submit'
+								type='apply'
+								onClick={handleApply}
+							/>
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
